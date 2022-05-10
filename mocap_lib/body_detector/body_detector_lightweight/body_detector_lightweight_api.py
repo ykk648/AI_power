20583a6999a62268b85362deb2a97aa568397f9c
luanjintai <tylerluan@163.com>
2022-05-10 14:57:18 +0800

    add mmhuman3d fbx output script

--- mocap_lib/body_detector/body_detector_lightweight/body_detector_lightweight_api.py
+++ mocap_lib/body_detector/body_detector_lightweight/body_detector_lightweight_api.py
@@ -41,6 +41,19 @@ class BodyDetectorLightweight:
                                         cv2.BORDER_CONSTANT, value=pad_value)
         return padded_img, pad
 
+    # def post_process(self):
+    #     total_keypoints_num = 0
+    #     all_keypoints_by_type = []
+    #     num_keypoints = 18
+    #     for kpt_idx in range(num_keypoints):  # 19th for bg
+    #         total_keypoints_num += extract_keypoints(heatmaps[:, :, kpt_idx], all_keypoints_by_type,
+    #                                                  total_keypoints_num)
+    #
+    #     pose_entries, all_keypoints = group_keypoints(all_keypoints_by_type, pafs, demo=True)
+    #     for kpt_id in range(all_keypoints.shape[0]):
+    #         all_keypoints[kpt_id, 0] = (all_keypoints[kpt_id, 0] * stride / upsample_ratio - pad[1]) / scale
+    #         all_keypoints[kpt_id, 1] = (all_keypoints[kpt_id, 1] * stride / upsample_ratio - pad[0]) / scale
+
     def forward(self, img):
         height, width, _ = img.shape
         scale = self.input_height_size / height
@@ -61,7 +74,7 @@ class BodyDetectorLightweight:
         pafs = np.transpose(stage2_pafs.squeeze(), (1, 2, 0))
         pafs = cv2.resize(pafs, (0, 0), fx=self.upsample_ratio, fy=self.upsample_ratio, interpolation=cv2.INTER_CUBIC)
 
-        return heatmaps, pafs
+        return heatmaps, pafs, scale, pad
 
 if __name__ == '__main__':
     img_p = 'test_img/t_pose.jpeg'
